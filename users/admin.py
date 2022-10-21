773e49a284490380b7de0c27a1c3bc4d0170e436
dennis mbithi <denmbithi@gmail.com>
2022-10-21 19:02:56 +0300

    Created custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom User Information",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birth_date",
+                    "language",
+                    "currency",
+                    "superhost"
+                )
+            }
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom user model
+    """
+
+    # Gender choices
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+
+    GENDER_CHOICES = ((GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Femal'), (GENDER_OTHER, 'Other'))
+
+    # Language choices
+    LANGUAGE_ENGLISH = 'en'
+    LANGUAGE_KOREAN = 'kr'
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, 'English'), (LANGUAGE_KOREAN, 'Korean'))
+
+    # Currency choices
+    CURRENCY_USD = 'USD'
+    CURRENCY_KOREAN = 'KRW'
+
+    CURRENCY_CHOICES = ((CURRENCY_KOREAN, 'Korean'),(CURRENCY_USD, 'USD'))
+
+
+    avatar      = models.ImageField(blank=True)
+    bio         = models.TextField(blank=True)
+    gender      = models.CharField(choices=GENDER_CHOICES, max_length=10)
+    birth_date  = models.DateField(null=True, blank=True)
+    language    = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency    = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost   = models.BooleanField(default=False)
