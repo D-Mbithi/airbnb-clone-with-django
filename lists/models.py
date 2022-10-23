58b8f1506f52ee4f9eb0634ed1d6e6a5d3de1d52
dennis mbithi <denmbithi@gmail.com>
2022-10-23 13:53:58 +0300

    Lists model implementation

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from core.models import TimeStampedModel
+
 
 # Create your models here.
+class List(TimeStampedModel):
+    name = models.CharField(max_length=140)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    rooms = models.ManyToManyField("rooms.Room")
+
+    def __str__(self):
+        return self.name
