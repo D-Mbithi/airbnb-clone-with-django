f81972a6fe7130065c1292cc02db3058e2ad1c2b
dennis mbithi <denmbithi@gmail.com>
2022-10-26 10:12:37 +0300

    Added doc string for models.

--- core/models.py
+++ core/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class TimeStampedModel(models.Model):
-    """
-    Time stamped model.
-    """
+    """ Time stamped model. """
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
