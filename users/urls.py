e389989f248c4ede2ff284f546d14888fc3cab98
dennis mbithi <denmbithi@gmail.com>
2023-01-12 08:39:04 +0300

    Manaul way user authentication.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
 
+from users.views import LoginView, logout_view
 
 app_name = "users"
 
-
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views.generic import View
+
+from . import forms
+
 
 # Create your views here.
+class LoginView(View):
+    """Login View defined"""
+
+    def get(self, request):
+        form = forms.LoginForms()
+        template = "users/login.html"
+        context = {
+            "form": form,
+        }
+        return render(request, template, context)
+
+    def post(self, request):
+        form = forms.LoginForms(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+        template = "users/login.html"
+        context = {
+            "form": form,
+        }
+
+        return render(request, template, context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("users:login"))
